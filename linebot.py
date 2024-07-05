48840db778ca69adf5b353846c72168d4e2edb53
bimantorow <bimantoro.widyadana@ui.ac.id>
2024-07-06 01:46:59 +0700

    add initial send message method

--- linebot.py
+++ linebot.py
@@ -1,11 +1,14 @@
 import hmac
 import base64
 import hashlib
+import requests
 from fastapi import Request
+from pprint import pprint
 
 class LineBot:
-    def __init__(self, channel_secret: str) -> None:
+    def __init__(self, channel_secret: str, access_token: str) -> None:
         self.channel_secret = channel_secret
+        self.access_token = access_token
     
     async def is_valid_signature(self, request: Request):
         line_signature = request.headers.get("x-line-signature", None)
@@ -15,3 +18,33 @@ class LineBot:
         hash = hmac.new(self.channel_secret.encode(), body, hashlib.sha256).digest()
         signature = base64.b64encode(hash).decode()
         return line_signature == signature
+    
+    async def execute(self, request: Request):
+        endpoint = "https://api.line.me/v2/bot/message/reply"
+        body = await request.json()
+
+        for event in body["events"]:
+            if event["type"] == "message":
+                reply_token = event["replyToken"]
+                message = event["message"]["text"]
+
+                headers = {
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {self.access_token}"
+                }
+                data = {
+                    "replyToken": reply_token,
+                    "messages": [
+                        {
+                            "type": "text",
+                            "text": message
+                        }
+                    ]
+                }
+
+                response = requests.post(
+                    endpoint,
+                    headers=headers,
+                    json=data
+                )
+                pprint(response.json())
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ load_dotenv()
 
 app = FastAPI()
 channel_secret = os.getenv("CHANNEL_SECRET")
-linebot = LineBot(channel_secret)
+channel_access_token = os.getenv("CHANNEL_ACCESS_TOKEN")
+linebot = LineBot(channel_secret, channel_access_token)
 
 @app.get("/")
 async def root():
@@ -18,6 +19,7 @@ async def root():
 async def callback(request: Request):
     is_valid_signature = await linebot.is_valid_signature(request)
     if is_valid_signature:
+        await linebot.execute(request)
         return ""
     else:
         raise HTTPException(status_code=403, detail="Invalid LINE signature")
