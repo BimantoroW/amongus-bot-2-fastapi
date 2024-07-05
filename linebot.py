9eee5de4b7f995bfad1d870dd7f483a097843fb3
bimantorow <bimantoro.widyadana@ui.ac.id>
2024-07-05 20:08:52 +0000

    add among us cock and leave features

--- linebot.py
+++ linebot.py
@@ -5,6 +5,12 @@ import requests
 from fastapi import Request
 from pprint import pprint
 
+
+proxies = {
+    "http": "proxy.server:3128",
+    "https": "proxy.server:3128"
+}
+
 class LineBot:
     def __init__(self, channel_secret: str, access_token: str) -> None:
         self.channel_secret = channel_secret
@@ -25,26 +31,54 @@ class LineBot:
 
         for event in body["events"]:
             if event["type"] == "message":
-                reply_token = event["replyToken"]
                 message = event["message"]["text"]
+                if message.lower() == "among us cock":
+                    reply_token = event["replyToken"]
+                    headers = {
+                        "Content-Type": "application/json",
+                        "Authorization": f"Bearer {self.access_token}"
+                    }
+                    data = {
+                        "replyToken": reply_token,
+                        "messages": [
+                            {
+                                "type": "image",
+                                "originalContentUrl": "https://i.ytimg.com/vi/d94Nz9s3VBc/sddefault.jpg?v=5f73ada9",
+                                "previewImageUrl": "https://i.ytimg.com/vi/d94Nz9s3VBc/sddefault.jpg?v=5f73ada9"
+                            }
+                        ]
+                    }
 
-                headers = {
-                    "Content-Type": "application/json",
-                    "Authorization": f"Bearer {self.access_token}"
-                }
-                data = {
-                    "replyToken": reply_token,
-                    "messages": [
-                        {
-                            "type": "text",
-                            "text": message
+                    response = requests.post(
+                        endpoint,
+                        headers=headers,
+                        json=data,
+                        proxies=proxies
+                    )
+                    pprint(response.json())
+                elif message.lower() == "/leave":
+                    type = event["source"]["type"]
+                    if type == "group":
+                        group_id = event["source"]["groupId"]
+                        endpoint = f"https://api.line.me/v2/bot/group/{group_id}/leave"
+                        headers = {
+                            "Authorization": f"Bearer {self.access_token}"
                         }
-                    ]
-                }
-
-                response = requests.post(
-                    endpoint,
-                    headers=headers,
-                    json=data
-                )
-                pprint(response.json())
+                        response = requests.post(
+                            endpoint,
+                            headers=headers,
+                            proxies=proxies
+                        )
+                        pprint(reponse.json())
+                    elif type == "room":
+                        room_id = event["source"]["roomId"]
+                        endpoint = f"https://api.line.me/v2/bot/room/{room_id}/leave"
+                        headers = {
+                            "Authorization": f"Bearer {self.access_token}"
+                        }
+                        response = requests.post(
+                            endpoint,
+                            headers=headers,
+                            proxies=proxies
+                        )
+                        pprint(reponse.json())
